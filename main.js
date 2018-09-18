da56568de74d3c655452b23cc144e0194d5a75da
avicndugu <avicnduguh@gmail.com>
2018-09-18 21:43:14 +0300

    Added all files images clickable and linked to js

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('.rock').addEventListener("click", function(){
+  alert("rock");
+});
+document.querySelector('.paper').addEventListener("click", function(){
+    alert("paper");
+  });
+document.querySelector('.scissors').addEventListener("click", function(){
+    alert("scissors");
+  });
+// alert("Hell");
